c0a213481abe239062ad84046e495a3d7460eff6
Tamirlan Dzhemirzoev <tamirlan.dzhemirzoev@ccsteam.ru>
2021-08-12 18:40:02 +0300

    Ref: Change 'Pagination' widget implementation to class

--- pybotx_widgets/base.py
+++ pybotx_widgets/base.py
@@ -1,6 +1,32 @@
-from typing import Any, Optional
+from typing import Any, Optional, Union
+from uuid import UUID, uuid4
 
-from botx import Bot, Message, MessageMarkup
+from botx import (
+    Bot,
+    Message,
+    MessageMarkup,
+    SendingCredentials,
+    SendingMessage,
+    UpdatePayload,
+)
+
+
+class WidgetMarkup:
+    markup: MessageMarkup
+    additional_markup: Optional[MessageMarkup]
+
+    def add_additional_markup(self) -> None:
+        if self.additional_markup:
+            self.markup = self.merge_markup(self.markup, self.additional_markup)
+
+    @classmethod
+    def merge_markup(
+        cls, primary: MessageMarkup, additional: MessageMarkup
+    ) -> MessageMarkup:
+        return MessageMarkup(
+            bubbles=(primary.bubbles + additional.bubbles),
+            keyboard=(primary.keyboard + additional.keyboard),
+        )
 
 
 class Widget:
@@ -22,9 +48,34 @@ class Widget:
         self.command = command
         self.additional_markup = additional_markup
 
+        self.markup = MessageMarkup()
+
     async def display(self) -> Optional[Any]:
         raise NotImplementedError
 
-    @classmethod
-    async def get_value(cls, *args, **kwargs) -> Any:
-        raise NotImplementedError
+    async def send_or_update_message(
+        self, widget_msg: SendingMessage, new_message_id: Union[str, UUID] = None
+    ) -> None:
+        """Send new message or update exist."""
+
+        current_message_id = self.message.data.get("message_id")
+        new_message_id = new_message_id or uuid4()
+
+        for bubbles in widget_msg.markup.bubbles:
+            for bubble in bubbles:
+                bubble.data["message_id"] = current_message_id or new_message_id
+
+        if current_message_id:
+            payload = UpdatePayload(text=widget_msg.text, file=widget_msg.file)
+            payload.set_markup(markup=widget_msg.markup)
+            await self.bot.update_message(
+                SendingCredentials(
+                    sync_id=current_message_id,
+                    bot_id=self.message.bot_id,
+                    host=self.message.host,
+                ),
+                update=payload,
+            )
+        else:
+            widget_msg.credentials.message_id = new_message_id
+            await self.bot.send(widget_msg)
